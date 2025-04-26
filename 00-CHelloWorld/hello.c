3a373775d3afb326ced329bf6c739d1350269be8
FeaR <fear@localhost.localdomain>
2025-04-26 12:19:43 -0300

    Ejercicio 3 Hello World

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define EXIT_SUCCESS 0
+
+
+int main(void){
+  printf("Hello, World!\n");
+
+  return EXIT_SUCCESS;
+}
---
+++ 00-CHelloWorld/readme.md
@@ -0,0 +1,6 @@
+# Hello World
+Ejercicio 3 "Hello, World!" en C
+
+gcc
+12.2.0
+c2x
